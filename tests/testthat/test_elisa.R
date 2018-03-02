218894015ed13b34cc3f95e5127c52446bf92f9d
Eric Koncina <mail@koncina.eu>
2018-03-02 19:13:26 +0100

    update testthat validation (elisa and plot)

--- tests/testthat/test_elisa.R
+++ tests/testthat/test_elisa.R
@@ -1,8 +1,9 @@
 context("elisa_analyse")
 
 test_that("performing the 4-PL regression without custom arguments generates the appropriate values", {
-  x <- read_plate("example_full.xls", na = "Empty")
-  x <- suppressWarnings(elisa_analyse(x))
+  od_data <- read_plate("example_full.xls", na = "Empty")
+  od_data <- od_data[!is.na(od_data$id),]
+  x <- suppressWarnings(elisa_analyse(od_data))
   x <- apply(x[,c("value", "estimate")], 2, sum)
   x["estimate"] <- floor(x["estimate"])
   expect_equivalent(x, c(value = 26.364, estimate = 12534))
@@ -10,7 +11,8 @@ test_that("performing the 4-PL regression without custom arguments generates the
 
 test_that("performing the 4-PL regression and back the estimate as a vector works", {
   od_data <- read_plate("example_full.xls", na = "Empty")
-  x <- suppressWarnings(get_concentration(id = od_data[["id"]], value = od_data[["value"]]))
+  od_data <- od_data[!is.na(od_data$id),]
+  x <- suppressWarnings(od_to_concentration(id = od_data[["id"]], value = od_data[["value"]]))
   expect_equivalent(length(x), nrow(od_data))
   x <- floor(sum(x))
   expect_equivalent(x, 12534)
--- tests/testthat/test_plot.R
+++ tests/testthat/test_plot.R
@@ -1,38 +1,22 @@
 context("plot")
 
 test_that("using the stat_4pl layer renders a plot", {
-  library(dplyr)
   library(ggplot2)
   library(scales)
-  read.plate("example_full.xls") %>%
-    elisa.standard() %>%
-    ggplot(aes(x = x, y = value)) +
+  od_data <- read_plate("example_full.xls", na = "Empty")
+  std <- extract_standard(od_data)
+  ggplot(std, aes(x = x, y = y)) +
     geom_point() +
     stat_4pl() -> plot.test
   expect_is(plot.test, "ggplot")
 })
 
-test_that("setting stat_4pl to verbose renders a plot and a message", {
-  library(dplyr)
-  library(ggplot2)
-  library(scales)
-  
-  p <- read.plate("example_full.xls") %>%
-    elisa.standard() %>%
-    ggplot(aes(x = x, y = value)) +
-    geom_point()
-  
-  expect_message(p + stat_4pl(verbose = TRUE), "^Showing 4PL model details")
-  expect_is(p + stat_4pl(verbose = TRUE), "ggplot")
-})
-
 test_that("setting the stat_4pl logDose option still renders a plot", {
-  library(dplyr)
   library(ggplot2)
   library(scales)
-  p <- read.plate("example_full.xls") %>%
-    elisa.standard() %>%
-    ggplot(aes(x = x, y = value)) +
+  od_data <- read_plate("example_full.xls", na = "Empty")
+  std <- extract_standard(od_data)
+  p <- ggplot(std, aes(x = x, y = value)) +
     geom_point()
   expect_is(p + stat_4pl(logDose = 10), "ggplot")
 })
